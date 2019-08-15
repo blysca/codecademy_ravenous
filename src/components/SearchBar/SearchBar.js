8341c3f3fc3b8d32f271ea6338fd451a0b1b26ae
blysca <blysca@gmail.com>
2019-08-15 21:11:38 +0300

    Create an Options Object

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './SearchBar.css';
+import Business from '../BusinessList/BusinessList';
+
+const sortByOptions = {
+  'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Reviewed': 'review_count'
+};
+
+class SearchBar extends React.Component {
+  render() {
+    return;
+  }
+}
+
+export default SearchBar;
